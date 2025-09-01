201ce4674b7fba6055093f43208f16e61ec58aad
ninarada <nradja00@fesb.hr>
2025-09-01 18:07:18 +0200

    .

--- routes/receiptRoutes.js
+++ routes/receiptRoutes.js
@@ -3,12 +3,9 @@ const router = express.Router();
 const { createReceipt, updateReceipt, getReceipts, getCategoryItems, getReceiptById, deleteReceipt } = require('../controllers/receiptController');
 const { protect } = require('../middleware/authMiddleware');
 
-// Public routes
-
-// Private routes (requires authentication)
 router.post('/new', protect, createReceipt);                    // POST /api/receipts/new - Create a new receipt
 router.put('/update/:id', protect, updateReceipt);              // PUT /api/receipts/update/:id
-router.delete('/deleteReceipt', protect, deleteReceipt);         // DELETE /api/receipts/deleteReceipt
+router.delete('/deleteReceipt', protect, deleteReceipt);        // DELETE /api/receipts/deleteReceipt
 router.get('/getAll', protect, getReceipts);                    // GET /api/receipts/getAll - Get all receipts for the logged-in user
 router.get('/getCategoryItems', protect, getCategoryItems);     // GET /api/receipts/getCategory - Get all items for the selected category
 router.get('/:id', protect, getReceiptById);                    // GET /api/receipts/:id 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,21 +15,16 @@ const {
 const { protect } = require('../middleware/authMiddleware');
 const upload = require("../middleware/upload");
 
-// Public routes
 router.post('/register', registerUser);         // POST /api/users/register - Register a new user
 router.post('/login', authUser);                // POST /api/users/login - Login user
 
-// Private route (requires authentication)
-router.get('/profile', protect, getMyProfile);              // GET /api/users/profile - Get user profile
-router.get('/stats', protect, getUserStats);                // GET /api/users/stats - Get user stats (for profile)
-
+router.get('/profile', protect, getMyProfile);                                      // GET /api/users/profile - Get user profile
+router.get('/stats', protect, getUserStats);                                        // GET /api/users/stats - Get user stats (for profile)
 router.put('/updateProfile', protect, upload.single("photo"), updateMyProfile);     // PUT /api/users/updateProfile
-router.put('/changePassword', protect, changePassword);                             //PUT /api/users/changePassword
-
-router.post('/newCategory', protect, createCategory);       // POST /api/users/newCategory - Create a new category
-router.post('/addCategoryToFavourites', protect, addCategoryToFavourites);         // POST /api/users/addCategoryToFavourites
-
-router.delete('/deleteCategory', protect, deleteCategory);  // DELETE /api/users/deleteCategory?name=Groceries - Delete selected category
-router.delete('/deleteUser', protect, deleteUser);          // DELETE /api/users/deleteUser
+router.put('/changePassword', protect, changePassword);                             // PUT /api/users/changePassword
+router.post('/newCategory', protect, createCategory);                               // POST /api/users/newCategory - Create a new category
+router.post('/addCategoryToFavourites', protect, addCategoryToFavourites);          // POST /api/users/addCategoryToFavourites
+router.delete('/deleteCategory', protect, deleteCategory);                          // DELETE /api/users/deleteCategory?name=Groceries - Delete selected category
+router.delete('/deleteUser', protect, deleteUser);                                  // DELETE /api/users/deleteUser
 
 module.exports = router;
